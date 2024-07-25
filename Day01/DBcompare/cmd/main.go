5fc8a3fc9403c29f23ed00d2c902b152b48b4308
Sonia Leena <sonialee@pr-b4.kzn.21-school.ru>
2024-07-25 04:25:30 +0300

    fixed

--- Day01/DBcompare/cmd/main.go
+++ Day01/DBcompare/cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	pkg "Day01/pkg/dbreader"
+	"flag"
+)
+
+func main() {
+	var data1file, data2file string
+	flag.StringVar(&data1file, "data1", "", "First database file name")
+	flag.StringVar(&data2file, "data2", "", "Second database file name")
+	flag.Parse()
+	data1, _ := pkg.FileformatRead(data1file)
+	data2, _ := pkg.FileformatRead(data2file)
+}
