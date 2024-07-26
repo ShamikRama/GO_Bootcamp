69cc5e1d6410647464545f7cd1924ddd200b9c00
Sonia Leena <sonialee@pr-c6.kzn.21-school.ru>
2024-07-26 23:12:01 +0300

    added main

--- Day01/FScompare/cmd/main.go
+++ Day01/FScompare/cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	pkg_s "Day01/pkg/fscompare"
+	"flag"
+)
+
+func main() {
+	var snap1, snap2 string
+	flag.StringVar(&snap1, "old", "", "First snap file name")
+	flag.StringVar(&snap2, "new", "", "Second snap file name")
+	flag.Parse()
+	old, _ := pkg_s.GetFiles(snap1)
+	new, _ := pkg_s.GetFiles(snap2)
+	pkg_s.Compare(old, new)
+}
