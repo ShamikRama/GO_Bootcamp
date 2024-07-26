99803ea630e2bcfe653005c5c95aab9434e71618
Sonia Leena <sonialee@pr-c6.kzn.21-school.ru>
2024-07-27 00:37:14 +0300

    fixed

--- Day01/pkg/fscompare/struct_funcs.go
+++ Day01/pkg/fscompare/struct_funcs.go
@@ -3,17 +3,12 @@ package pkg
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"slices"
 )
 
 func GetFiles(filesnap string) (files []string, err error) {
-	file, err := os.Open(filesnap)
-	if err != nil {
-		log.Fatal("Error opening file:", err)
-		return
-	}
+	file, _ := os.Open(filesnap)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -21,30 +16,17 @@ func GetFiles(filesnap string) (files []string, err error) {
 		files = append(files, filename)
 	}
 	err = scanner.Err()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return files, err
-	}
 	return files, err
 }
 
-func Compare(firstsnap, secondsnap string) {
-	oldfile, err := GetFiles(firstsnap)
-	if err != nil {
-		fmt.Println("Error getting file")
-	}
-	newfile, err := GetFiles(secondsnap)
-	if err != nil {
-		fmt.Println("Error getting file")
-	}
-
-	for _, val := range oldfile {
-		if !slices.Contains(newfile, val) {
+func Compare(firstsnap, secondsnap []string) {
+	for _, val := range firstsnap {
+		if !slices.Contains(secondsnap, val) {
 			fmt.Print("Added:", val)
 		}
 	}
-	for _, val := range newfile {
-		if !slices.Contains(oldfile, val) {
+	for _, val := range secondsnap {
+		if !slices.Contains(firstsnap, val) {
 			fmt.Print("Removed:", val)
 		}
 	}
