267541e31e6006829f0b27d70daf032f8364a4f0
Sonia Leena <sonialee@pr-c6.kzn.21-school.ru>
2024-07-27 02:05:56 +0300

    fixed

--- Day01/pkg/fscompare/struct_funcs.go
+++ Day01/pkg/fscompare/struct_funcs.go
@@ -7,18 +7,20 @@ import (
 	"slices"
 )
 
-func GetFiles(filesnap string) (files []string, err error) {
+// get text from file
+func GetFiles(filesnap string) (text []string, err error) {
 	file, _ := os.Open(filesnap)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		filename := scanner.Text()
-		files = append(files, filename)
+		text = append(text, filename)
 	}
 	err = scanner.Err()
-	return files, err
+	return text, err
 }
 
+// compare 1.txt and 2.txt
 func Compare(firstsnap, secondsnap []string) {
 	for _, val := range firstsnap {
 		if !slices.Contains(secondsnap, val) {
