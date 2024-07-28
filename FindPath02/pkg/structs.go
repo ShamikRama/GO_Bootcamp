f82d1e7acfdd9bf7f76b071f2128e52ae831f0a3
Sonia Leena <sonialee@pr-o4.kzn.21-school.ru>
2024-07-28 22:10:14 +0300

    fixed

--- FindPath02/pkg/structs.go
+++ FindPath02/pkg/structs.go
@@ -1,7 +1,9 @@
 package pkg
 
 import (
+	"errors"
 	"flag"
+	"os"
 )
 
 type Flags struct {
@@ -20,12 +22,54 @@ func FindFlagAndArg() (fl Flags, arg Args, err error) {
 	flag.BoolVar(&fl.Dironly, "d", false, "Only directory")
 	flag.BoolVar(&fl.Simonly, "sl", false, "Only symlinks")
 	flag.BoolVar(&fl.Fileonly, "f", false, "Only filenames")
-	flag.StringVar(&arg.Ext, "ext", "", "Specification of file")
+	flag.StringVar(&arg.Ext, "ext", "", "Specification of file extension")
+	flag.StringVar(&arg.DirPath, "dir", "", "Directory path")
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) > 0 {
-		arg.Ext = args[0]
+	if arg.Ext != "" && !fl.Fileonly {
+		return fl, arg, ErrWrongFlagsCombination
 	}
-	return fl, arg, err
+
+	if arg.DirPath == "" {
+		return fl, arg, ErrNoDirPassed
+	}
+
+	if _, err := os.Stat(arg.DirPath); os.IsNotExist(err) {
+		return fl, arg, ErrNoSuchDirectory
+	}
+
+	return fl, arg, nil
+}
+
+var ErrWrongFlagsCombination = errors.New("flag -ext works only with flag -f")
+var ErrNoSuchDirectory = errors.New("no such directory")
+var ErrNoDirPassed = errors.New("no directory passed")
+
+func Noflags(fl Flags) bool {
+	return !fl.Dironly && !fl.Simonly && !fl.Fileonly
+}
+
+func NoDirectory(arg Args) error {
+	if _, err := os.Stat(arg.DirPath); os.IsNotExist(err) {
+		return ErrNoSuchDirectory
+	}
+	return nil
+}
+
+func WrongCombiantion(fl Flags) error {
+	if fl.FileExtonly && !fl.Fileonly {
+		return ErrWrongFlagsCombination
+	}
+	return nil
+}
+
+func NoDirPass(arg Args) error {
+	if arg.DirPath == "" {
+		return ErrNoDirPassed
+	}
+	return nil
+}
+
+func NotNilExt(fl Flags, arg Args) bool {
+	return arg.Ext != ""
 }
