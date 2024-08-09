e709fe7bf0197dec2e2e2e1497bbbc8b90b91e1c
Sonia Leena <sonialee@pr-m5.kzn.21-school.ru>
2024-08-09 20:51:32 +0300

    added funcs

--- Data_API/interanl/db/esstore.go
+++ Data_API/interanl/db/esstore.go
@@ -1,6 +1,10 @@
 package db
 
 import (
+	m "Data_Api/models"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/elastic/go-elasticsearch/v8"
@@ -22,4 +26,22 @@ func NewESstore() (*ESstore, error) {
 	return &ESstore{
 		client: client,
 	}, nil
+}
+
+func (s *ESstore) IndexPlace(place m.Place) (err error) {
+	var buf bytes.Buffer
+	encoder := json.NewEncoder(&buf)
+	err = encoder.Encode(place)
+	if err != nil {
+		return fmt.Errorf("error encoding place: %s", err)
+	}
+	res, err := s.client.Index("places", &buf)
+	if err != nil {
+		return fmt.Errorf("error indexing document: %s", err)
+	}
+	defer res.Body.Close()
+	if res.IsError() {
+		return fmt.Errorf("error indexing document: %s", res.Status())
+	}
+	return nil
 }
