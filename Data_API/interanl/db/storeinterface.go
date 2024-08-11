63b8a9e6fecbfc4ed1d0a51f3cb7c3578743cb67
Sonia Leena <sonialee@un-b6.kzn.21-school.ru>
2024-08-11 16:04:33 +0300

    deleted

--- Data_API/interanl/db/storeinterface.go
+++
@@ -1,8 +0,0 @@
-package db
-
-import "Data_Api/models"
-
-type Store interface {
-	GetPlaces(limit, offset int) ([]models.Place, int, error)
-	GetNearestPlaces(lat, lon float64, limit int) ([]models.Place, error)
-}
