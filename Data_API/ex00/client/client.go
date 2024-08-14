d04d2b840aa4ca074cf5083fde09d548a4b533da
Sonia Leena <sonialee@pr-f4.kzn.21-school.ru>
2024-08-14 20:46:32 +0300

    fixed

--- Data_API/ex00/client/client.go
+++ Data_API/ex00/client/client.go
@@ -9,14 +9,15 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	i "Data_Api/internal"
 
-	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
 const (
-	shemapath string = "src/ex00/cmd/shema.json"
+	shemapath string = "shema.json"
 	url       string = "http://localhost:9200/"
 )
 
@@ -27,9 +28,10 @@ func GetConnectionClient() *elasticsearch.Client {
 			MaxIdleConnsPerHost: 100,
 			MaxConnsPerHost:     100,
 		},
+		Timeout: 30 * time.Second, // Увеличьте время ожидания
 	}
 	es, err := elasticsearch.NewClient(elasticsearch.Config{
-		Addresses: []string{"http://localhost:9200"},
+		Addresses: []string{url},
 		Transport: httpClient.Transport,
 	})
 	if err != nil {
@@ -39,14 +41,14 @@ func GetConnectionClient() *elasticsearch.Client {
 }
 
 // создание индекса
-func CreateIndex(indexName string, es *elasticsearch.Client) (err error) {
-	_, err = es.Indices.Delete([]string{indexName})
+func CreateIndex(indexName string, es *elasticsearch.Client) error {
+	_, err := es.Indices.Delete([]string{indexName})
 	if err != nil {
-		return err
+		return fmt.Errorf("error deleting index: %w", err)
 	}
 	res, err := es.Indices.Create(indexName)
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating index: %w", err)
 	}
 	if res.IsError() {
 		return fmt.Errorf("failed to create index: %s", res.String())
@@ -54,21 +56,21 @@ func CreateIndex(indexName string, es *elasticsearch.Client) (err error) {
 	return nil
 }
 
-// маппинг то есть создание структуры как будут хранится данные
+// маппинг то есть создание структуры как будут храниться данные
 func Mapping(nameIndex string, es *elasticsearch.Client) error {
 	shema := i.JsonReader{}
 	sh, err := shema.ReadDB(shemapath)
 	if err != nil {
-		return err
+		return fmt.Errorf("error reading schema: %w", err)
 	}
 	sh.Properties.Id.Type = "long"
 	shemabytes, err := json.Marshal(sh)
 	if err != nil {
-		fmt.Println("Error marshalling into shemabytes")
+		return fmt.Errorf("error marshalling schema: %w", err)
 	}
 	req, err := http.NewRequest(http.MethodPut, url+nameIndex+"/place/_mapping", bytes.NewBuffer(shemabytes))
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating request: %w", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
 	q := req.URL.Query()
@@ -78,30 +80,68 @@ func Mapping(nameIndex string, es *elasticsearch.Client) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return fmt.Errorf("error sending request: %w", err)
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
+
+// заполнение базы
+func FillData(indexname string, es *elasticsearch.Client, csvpath string) error {
+	data := i.CSVReader{}
+	lines, err := data.ReadDB(csvpath)
+	if err != nil {
+		return fmt.Errorf("error reading CSV: %w", err)
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var errors []error
+
+	for indx, val := range lines[1:] {
+		wg.Add(1)
+		id := indx
+		line := val
+		go func() {
+			defer wg.Done()
+			err := pushIntoES(indexname, id, line, es)
+			if err != nil {
+				mu.Lock()
+				errors = append(errors, err)
+				mu.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+
+	if len(errors) > 0 {
+		return fmt.Errorf("errors occurred during data push: %v", errors)
+	}
 
 	return nil
 }
 
 // загрузка данных в es
-func pushIntoES(indexname string, idx int, line []string, es *elasticsearch.Client) (err error) {
+func pushIntoES(indexname string, idx int, line []string, es *elasticsearch.Client) error {
 	id := strconv.Itoa(idx)
 
 	longitude, err := strconv.ParseFloat(line[4], 64)
 	if err != nil {
-		fmt.Printf("Error parsing to float")
+		return fmt.Errorf("error parsing longitude: %w", err)
 	}
 
 	latitude, err := strconv.ParseFloat(line[5], 64)
 	if err != nil {
-		fmt.Printf("Error parsing to float")
+		return fmt.Errorf("error parsing latitude: %w", err)
 	}
 
 	place := i.Place{
-		Address: line[2],
 		Id:      idx,
+		Address: line[2],
 		Location: i.Location{
 			Longitude: longitude,
 			Latitude:  latitude,
@@ -112,41 +152,16 @@ func pushIntoES(indexname string, idx int, line []string, es *elasticsearch.Clie
 
 	data, err := json.Marshal(place)
 	if err != nil {
-		return err
+		return fmt.Errorf("error marshalling place: %w", err)
 	}
 
 	resp, err := es.Index(indexname, strings.NewReader(string(data)), es.Index.WithDocumentID(id))
 	if err != nil {
-		return err
+		return fmt.Errorf("error indexing document ID=%s: %w", id, err)
 	}
 	defer resp.Body.Close()
-
-	return nil
-
-}
-
-// заполнение базы
-func fillESDATA(indexname string, es *elasticsearch.Client, csvpath string) (err error) {
-	data := i.CSVReader{}
-	lines, err := data.ReadDB(csvpath)
-	if err != nil {
-		return nil
+	if resp.IsError() {
+		return fmt.Errorf("error indexing document ID=%s: %s", id, resp.String())
 	}
-	var wg sync.WaitGroup
-
-	for indx, val := range lines[1:] {
-		wg.Add(1)
-		id := indx
-		line := val
-		go func() {
-			defer wg.Done()
-			err = pushIntoES(indexname, id, line, es)
-			if err != nil {
-				fmt.Println("Error push data inro elastic:", err)
-			}
-
-		}()
-	}
-	wg.Wait()
 	return nil
 }
